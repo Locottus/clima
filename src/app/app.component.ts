0ef3ac3bc66354a06da5651f6c036c70cb350492
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-08-26 15:05:42 -0600

    controles cargan datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,102 +1,159 @@
-import {Component, ElementRef, ViewChild,  Injectable} from '@angular/core';
-import {loadModules} from 'esri-loader';
-import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild, Injectable } from "@angular/core";
+import { loadModules } from "esri-loader";
+import { HttpClient } from "@angular/common/http";
 
 //https://swimlane.github.io/ngx-charts/#/ngx-charts/line-chart
 
 //var stamm = "https://arcgis-web.url.edu.gt/incyt/api/sosguate";
-var stamm = "http://localhost:3004/incyt/api/clima"
+var stamm = "http://localhost:3004/incyt/api/clima";
 var estations;
 var years;
 var months;
 
-  @Injectable()
-  export class ConfigService {
-    constructor(private http: HttpClient) { }
-  }
-
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) {}
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-
-
-
-
 export class AppComponent {
   // Get a container link for map place
-  @ViewChild('mapView', {static: true}) private readonly mapViewElement: ElementRef;
+  @ViewChild("mapView", { static: true })
+  private readonly mapViewElement: ElementRef;
   // main map view
   private mapView;
-  title = 'ArcGIS angular map Template';
+  title = "ArcGIS angular map Template";
+
+  cargaEstacion() {
+    console.log("carga estaciones");
+    var select = document.getElementById("selectEstacion");
+    console.log(estations);
+    for (var i = 0; i < estations.length; i++) {
+      var el = document.createElement("option");
+      el.textContent = estations[i].estacion;
+      el.value = estations[i].estacion;
+      select.appendChild(el);
+    }
+  }
+
+  cargaFechas(opcion) {
+    console.log("carga fechas");
+    var objeto = "";
+    if (opcion === 1) objeto = "selectyyyy1";
+    if (opcion === 2) objeto = "selectmm1";
+    if (opcion === 3) objeto = "selectyyyy2";
+    if (opcion === 4) objeto = "selectmm2";
+
+    var select = document.getElementById(objeto);
+    if (opcion === 1 ) {
+      for (var i = years.length - 1; i >= 0 ; i--) {
+        var el = document.createElement("option");
+        el.textContent = years[i].year;
+        el.value = years[i].year;
+        select.appendChild(el);
+      }
+    }
+    if ( opcion === 3) {
+      for (var i = 0 ; i < years.length ; i++) {
+        var el = document.createElement("option");
+        el.textContent = years[i].year;
+        el.value = years[i].year;
+        select.appendChild(el);
+      }
+    }
+
+    if (opcion === 2 || opcion === 4) {
+      for (var i = 0; i < months.length; i++) {
+        var el = document.createElement("option");
+        el.textContent = months[i].mes;
+        el.value = months[i].id;
+        select.appendChild(el);
+      }
+    }
+  }
 
   //DUMMY FUNCTION GET
-  httpGetFunctionOnInit(url,option) {
-    this.http.get(url)
-      .subscribe(
-        (val) => {
-          //console.log("GET call successful value returned in body", val);
-          if (option === 1)
-            estations = val;
-          if (option === 2)
-            years = val;
-          if (option === 3)
-            months = val;
+  httpGetFunctionOnInit(url, option) {
+    this.http.get(url).subscribe(
+      (val) => {
+        console.log("GET call successful value returned in body", val);
+        if (option === 1) {
+          estations = val;
+          this.cargaEstacion();
+        }
 
-        },
-        response => {
-          console.log("GET call in error", response);
-        },
-        () => {
-          console.log("The GET observable is now completed.");
-        });
+        if (option === 2) {
+          years = val;
+          this.cargaFechas(1);
+          this.cargaFechas(3);
+        }
+
+        if (option === 3) {
+          months = val;
+          this.cargaFechas(2);
+          this.cargaFechas(4);
+        }
+      },
+      (response) => {
+        console.log("GET call in error", response);
+      },
+      () => {
+        console.log("The GET observable is now completed.");
+      }
+    );
   }
 
   //DUMMY FUNCTION POST
   httpPostFunction(url) {
-    this.http.post(url,
-      {
-        "nombre": "herlich",
-        "telefono": "telefono",
-        "msg": "hola mundo!",
-        "email": "ich@gmail.com"
+    this.http
+      .post(url, {
+        nombre: "herlich",
+        telefono: "telefono",
+        msg: "hola mundo!",
+        email: "ich@gmail.com",
       })
       .subscribe(
         (val) => {
-          console.log("POST call successful value returned in body",
-            val);
+          console.log("POST call successful value returned in body", val);
         },
-        response => {
+        (response) => {
           console.log("POST call in error", response);
         },
         () => {
           console.log("The POST observable is now completed.");
-        });
+        }
+      );
   }
 
-
   constructor(private http: HttpClient) {
+    //Staadten detail Dienst addresse
+    var url = stamm + "/getestaciones";
+    this.httpGetFunctionOnInit(url, 1);
 
-  //Staadten detail Dienst addresse
-  var url = stamm  + "/getestaciones";
-  this.httpGetFunctionOnInit(url,1);
-  url = stamm  + "/getanios";
-  this.httpGetFunctionOnInit(url,2);
-  url = stamm  + "/getmeses";
-  this.httpGetFunctionOnInit(url,3);
-
-
+    url = stamm + "/getanios";
+    this.httpGetFunctionOnInit(url, 2);
+    url = stamm + "/getmeses";
+    this.httpGetFunctionOnInit(url, 3);
 
     // This function to load Dojo's require the classes listed in the array modules
-    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/MapImageLayer' ])
-  .then(([Map, MapView, MapImageLayer]: [__esri.MapConstructor, __esri.MapViewConstructor,
-        __esri.MapImageLayerConstructor]) => {
+    loadModules([
+      "esri/Map",
+      "esri/views/MapView",
+      "esri/layers/MapImageLayer",
+    ]).then(
+      ([Map, MapView, MapImageLayer]: [
+        __esri.MapConstructor,
+        __esri.MapViewConstructor,
+        __esri.MapImageLayerConstructor
+      ]) => {
         // set default map properties
-        
+
         const mapProperties = {
-          basemap: 'topo'
+          basemap: "topo",
         };
         // create map by default properties
         const map = new Map(mapProperties);
@@ -105,9 +162,9 @@ export class AppComponent {
         // zoom - default zoom parameter, value from 1 to 18
         const mapViewProperties = {
           container: this.mapViewElement.nativeElement,
-          center: [-90.625, 15.6],//this is the center of the map
-          zoom: 8,//zoom level
-          map
+          center: [-90.625, 15.6], //this is the center of the map
+          zoom: 8, //zoom level
+          map,
         };
         // create map view by default properties
         this.mapView = new MapView(mapViewProperties);
@@ -117,13 +174,15 @@ export class AppComponent {
         // sublayers - this are the settings for the inner layers of the service.
         // id = 1 it tell us that will be displayed only one layer with the identifier
         const oilSandLayerProperties = {
-          url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/OilSandsProjectBoundaries/MapServer',
-          sublayers: [{id: 1}]
+          url:
+            "https://sampleserver6.arcgisonline.com/arcgis/rest/services/OilSandsProjectBoundaries/MapServer",
+          sublayers: [{ id: 1 }],
         };
         // Create map image layer by properties
         const oilSandsLayer = new MapImageLayer(oilSandLayerProperties);
         // Adding a layer into map
         map.add(oilSandsLayer);
-      });
+      }
+    );
   }
 }
