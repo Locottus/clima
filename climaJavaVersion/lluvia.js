1b46219b9c9b0080a221e556f6a7a32a6c925472
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-10-06 17:56:31 -0600

    separating 1 and 2 stations

--- climaJavaVersion/lluvia.js
+++ climaJavaVersion/lluvia.js
@@ -14,30 +14,55 @@ var labels;
 var datasetLluvia;
 var color = Chart.helpers.color;
 
+async function fetchData2(
+  estacion,
+  estacion2,
+  yyyy1,
+  yyyy2,
+  selectVisualizacion,
+  ctx
+) {
+  console.log("comparing 2 stations");
+}
 
-async function fetchData(estacion,estacion2,yyyy1,yyyy2,selectVisualizacion,ctx) {
+async function fetchData(
+  estacion,
+  estacion2,
+  yyyy1,
+  yyyy2,
+  selectVisualizacion,
+  ctx
+) {
   //MESES
   var res;
   res = await fetch(stamm + "/getmeses");
   this.meses = await res.json();
-  var titulo = '';
-  var l =[];
+  var titulo = "";
+  var l = [];
   var d1 = [];
   var barChartData;
 
   //https://arcgis-web.url.edu.gt/incyt/api/clima/getdata?yyyy1=1979&yyyy2=1982&estacion=Alameda
   console.log(selectVisualizacion);
   if (selectVisualizacion === "Historico") {
-    titulo = 'Historico de Lluvia';
-    var url = stamm + "/getdata?yyyy1="+yyyy1+"&yyyy2=" + yyyy2 + "&estacion=" + estacion;
+    titulo = "Historico de Lluvia";
+    var url =
+      stamm +
+      "/getdata?yyyy1=" +
+      yyyy1 +
+      "&yyyy2=" +
+      yyyy2 +
+      "&estacion=" +
+      estacion;
     console.log(url);
     res = await fetch(url);
     this.data = await res.json();
-   
-    
-    for(var i = 0; i < this.data.length; i++){
-        l.push(this.data[i].year + '/' + this.data[i].mes + '/' +this.data[i].dia)
-        d1.push(this.data[i].lluvia);
+
+    for (var i = 0; i < this.data.length; i++) {
+      l.push(
+        this.data[i].year + "/" + this.data[i].mes + "/" + this.data[i].dia
+      );
+      d1.push(this.data[i].lluvia);
     }
     this.labels = l;
     this.datasetLluvia = d1;
@@ -46,69 +71,73 @@ async function fetchData(estacion,estacion2,yyyy1,yyyy2,selectVisualizacion,ctx)
 
     barChartData = {
       labels: labels,
-      datasets: [{
-        label: titulo,
-        backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-        borderColor: window.chartColors.red,
-        borderWidth: 1,
-        data: datasetLluvia
-      }]
+      datasets: [
+        {
+          label: titulo,
+          backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+          borderColor: window.chartColors.red,
+          borderWidth: 1,
+          data: datasetLluvia,
+        },
+      ],
     };
     console.log(barChartData);
-
-    
-
   } else if (selectVisualizacion === "Promedio") {
-    titulo = 'Promedio de Lluvia';
-     var url = stamm + "/getdataAVG?yyyy1="+yyyy1+"&yyyy2=" + yyyy2 + "&estacion=" + estacion;
-     console.log(url);
-     res = await fetch(url);
-     this.data = await res.json();
+    titulo = "Promedio de Lluvia";
+    var url =
+      stamm +
+      "/getdataAVG?yyyy1=" +
+      yyyy1 +
+      "&yyyy2=" +
+      yyyy2 +
+      "&estacion=" +
+      estacion;
+    console.log(url);
+    res = await fetch(url);
+    this.data = await res.json();
 
-     for(var i = 0; i < this.data.length; i++){
-      l.push(this.data[i].year + '/' + this.data[i].mes )
+    for (var i = 0; i < this.data.length; i++) {
+      l.push(this.data[i].year + "/" + this.data[i].mes);
       d1.push(this.data[i].lluvia);
-  }
-  this.labels = l;
-  this.datasetLluvia = d1;
-  console.log(this.labels);
-  console.log(this.datasetLluvia);
+    }
+    this.labels = l;
+    this.datasetLluvia = d1;
+    console.log(this.labels);
+    console.log(this.datasetLluvia);
 
-  barChartData = {
-    labels: labels,
-    datasets: [{
-      label: titulo,
-      backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-      borderColor: window.chartColors.red,
-      borderWidth: 1,
-      data: datasetLluvia
-    }]
-  };
-  console.log(barChartData);
-
-  }else  if (selectVisualizacion === "Proyeccion") {
-
-
+    barChartData = {
+      labels: labels,
+      datasets: [
+        {
+          label: titulo,
+          backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+          borderColor: window.chartColors.red,
+          borderWidth: 1,
+          data: datasetLluvia,
+        },
+      ],
+    };
+    console.log(barChartData);
+  } else if (selectVisualizacion === "Proyeccion") {
   }
 
   //console.log(this.meses);
   console.log(this.data);
   //display data
   window.myBar = new Chart(ctx, {
-    type: 'bar',
+    type: "bar",
     data: barChartData,
     options: {
       responsive: true,
       legend: {
-        position: 'top',
+        position: "top",
       },
       title: {
         display: true,
-        text: titulo
-      }
-    }
+        text: titulo,
+      },
+    },
   });
-
 }
 
 function reporte(id) {
@@ -168,7 +197,7 @@ function fillTable(data) {
   }
 }
 $(document).ready(function () {
-  var ctx = document.getElementById('canvas').getContext('2d');
+  var ctx = document.getElementById("canvas").getContext("2d");
 
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -187,8 +216,23 @@ $(document).ready(function () {
     this.yyyy2 +
     " visualizacion " +
     this.selectVisualizacion;
-    fetchData(this.estacion,this.estacion2,this.yyyy1,this.yyyy2,this.selectVisualizacion,ctx);
-   
-});
 
-//console.log(randomScalingFactor());
+  if (this.estacion2.length > 1)
+    fetchData(
+      this.estacion,
+      this.estacion2,
+      this.yyyy1,
+      this.yyyy2,
+      this.selectVisualizacion,
+      ctx
+    );
+  else
+    fetchData2(
+      this.estacion,
+      this.estacion2,
+      this.yyyy1,
+      this.yyyy2,
+      this.selectVisualizacion,
+      ctx
+    );
+});
--- climaJavaVersion/temperatura.js
+++ climaJavaVersion/temperatura.js
@@ -13,14 +13,31 @@ var data;
 
 var color = Chart.helpers.color;
 
+async function fetchData2(
+  estacion,
+  estacion2,
+  yyyy1,
+  yyyy2,
+  selectVisualizacion,
+  ctx
+) {
+  console.log("viene para comparar una segunda estacion");
+}
 
-async function fetchData(estacion,estacion2,yyyy1,yyyy2,selectVisualizacion,ctx) {
+async function fetchData(
+  estacion,
+  estacion2,
+  yyyy1,
+  yyyy2,
+  selectVisualizacion,
+  ctx
+) {
   //MESES
   var res;
   res = await fetch(stamm + "/getmeses");
   this.meses = await res.json();
-  var titulo = '';
-  var l =[];
+  var titulo = "";
+  var l = [];
   var d1 = [];
   var d2 = [];
   var d3 = [];
@@ -29,167 +46,167 @@ async function fetchData(estacion,estacion2,yyyy1,yyyy2,selectVisualizacion,ctx)
   //https://arcgis-web.url.edu.gt/incyt/api/clima/getdata?yyyy1=1979&yyyy2=1982&estacion=Alameda
   console.log(selectVisualizacion);
   if (selectVisualizacion === "Historico") {
-    titulo = 'Historico de Temperatura';
-    var url = stamm + "/getdata?yyyy1="+yyyy1+"&yyyy2=" + yyyy2 + "&estacion=" + estacion;
+    titulo = "Historico de Temperatura";
+    var url =
+      stamm +
+      "/getdata?yyyy1=" +
+      yyyy1 +
+      "&yyyy2=" +
+      yyyy2 +
+      "&estacion=" +
+      estacion;
     console.log(url);
     res = await fetch(url);
     this.data = await res.json();
-   
-    
-    for(var i = 0; i < this.data.length; i++){
-        l.push(this.data[i].year + '/' + this.data[i].mes + '/' +this.data[i].dia)
-        d1.push(this.data[i].tmax);
-        d2.push(this.data[i].tmin);
+
+    for (var i = 0; i < this.data.length; i++) {
+      l.push(
+        this.data[i].year + "/" + this.data[i].mes + "/" + this.data[i].dia
+      );
+      d1.push(this.data[i].tmax);
+      d2.push(this.data[i].tmin);
     }
-    console.log('***********************************');
-    console.log(d1);
-    console.log(d2);
-    console.log('***********************************');
+    // console.log('***********************************');
+    // console.log(d1);
+    // console.log(d2);
+    //console.log('***********************************');
     barChartData = {
       labels: l,
       datasets: [
         {
-        label: "temperatura maxima",
-        //backgroundColor: window.chartColors.red,
-        borderColor: window.chartColors.red,
-        borderWidth: 1,
-        data: d1
-      },
-      {
-        label: "temperatura minima",
-        //backgroundColor: window.chartColors.blue,
-        borderColor: window.chartColors.blue,
-        borderWidth: 1,
-        data: d2
-      }
-
-    ]
+          label: "temperatura maxima",
+          //backgroundColor: window.chartColors.red,
+          borderColor: window.chartColors.red,
+          borderWidth: 1,
+          data: d1,
+        },
+        {
+          label: "temperatura minima",
+          //backgroundColor: window.chartColors.blue,
+          borderColor: window.chartColors.blue,
+          borderWidth: 1,
+          data: d2,
+        },
+      ],
     };
     console.log(barChartData);
-
-    
-
   } else if (selectVisualizacion === "Promedio") {
-    titulo = 'Promedio de Temperatura';
-     var url = stamm + "/getdataAVG?yyyy1="+yyyy1+"&yyyy2=" + yyyy2 + "&estacion=" + estacion;
-     console.log(url);
-     res = await fetch(url);
-     this.data = await res.json();
+    titulo = "Promedio de Temperatura";
+    var url =
+      stamm +
+      "/getdataAVG?yyyy1=" +
+      yyyy1 +
+      "&yyyy2=" +
+      yyyy2 +
+      "&estacion=" +
+      estacion;
+    console.log(url);
+    res = await fetch(url);
+    this.data = await res.json();
 
-     for(var i = 0; i < this.data.length; i++){
-      l.push(this.data[i].year + '/' + this.data[i].mes )
+    for (var i = 0; i < this.data.length; i++) {
+      l.push(this.data[i].year + "/" + this.data[i].mes);
       d1.push(this.data[i].tmax);
       d2.push(this.data[i].tmin);
       d3.push(this.data[i].tPromedio);
-
-  }
-  this.labels = l;
-  this.datasetLluvia = d1;
-  console.log(this.labels);
-  console.log(this.datasetLluvia);
-
-  barChartData = {
-    labels: l,
-    datasets: [
-      {
-      label: "temperatura maxima",
-      //backgroundColor: window.chartColors.red,
-      borderColor: window.chartColors.red,
-      borderWidth: 1,
-      data: d1
-    },    {
-      label: "temperatura minima",
-      //backgroundColor: window.chartColors.blue,
-      borderColor: window.chartColors.blue,
-      borderWidth: 1,
-      data: d2
-    },
-    {
-      label: "temperatura promedio",
-      //backgroundColor: window.chartColors.black,
-      borderColor: window.chartColors.green,
-      borderWidth: 1,
-      data: d3
     }
-  
-  
-  
-  ]
-  };
-  console.log(barChartData);
+    this.labels = l;
+    this.datasetLluvia = d1;
+    // console.log(this.labels);
+    // console.log(this.datasetLluvia);
 
-  }else  if (selectVisualizacion === "Proyeccion") {
-
-
+    barChartData = {
+      labels: l,
+      datasets: [
+        {
+          label: "temperatura maxima",
+          //backgroundColor: window.chartColors.red,
+          borderColor: window.chartColors.red,
+          borderWidth: 1,
+          data: d1,
+        },
+        {
+          label: "temperatura minima",
+          //backgroundColor: window.chartColors.blue,
+          borderColor: window.chartColors.blue,
+          borderWidth: 1,
+          data: d2,
+        },
+        {
+          label: "temperatura promedio",
+          //backgroundColor: window.chartColors.black,
+          borderColor: window.chartColors.green,
+          borderWidth: 1,
+          data: d3,
+        },
+      ],
+    };
+    console.log(barChartData);
+  } else if (selectVisualizacion === "Proyeccion") {
   }
 
   //console.log(this.meses);
   console.log(this.data);
   //display data
   window.myBar = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: barChartData,
     options: {
       responsive: true,
       legend: {
-        position: 'top',
+        position: "top",
       },
       title: {
         display: true,
-        text: titulo
-      }
-    }
+        text: titulo,
+      },
+    },
   });
-
 }
 
-
 function reporte(id) {
-    //console.log("entrando a generar reporte");
-    //console.log(currentDepartment + ' ' + currentMunicipio + ' ' +currentMunicipioId);
+  //console.log("entrando a generar reporte");
+  //console.log(currentDepartment + ' ' + currentMunicipio + ' ' +currentMunicipioId);
   //https://github.com/chartjs/Chart.js
-  
-    var stamm = "https://arcgis-web.url.edu.gt/incyt/api/clima";
-    //var stamm = "http://localhost:3000/incyt/api/sosguate";
-      
-    var url =
-      stamm + "/getalertsdetailreport" +
-      "?id=" + id;
-  
-    $.get(url, function (data, status) {
-      //console.log("Data: " + data + "\nStatus: " + status);
-      if (data.length > 0) {
-        console.log("desplegamos grid");
-        //console.log(data[0].twitjson);
-        fillTable(data);
-      } else {
-        alert("No hay datos disponibles");
-      }
-    });
-  }
-  
-  
-  function fillTable(data) {
-    console.log(data);
-    var table = document.getElementById("tableInfo");
-    var tableTitle = document.getElementById("tableTitle");
-    tableTitle.innerHTML = "Reportes Municipales de Agua";
-    table.innerHTML = "";
-    table.innerHTML =
-      "<thead>" +
-      "   <tr>" +
-      "    <th scope='col'>Fecha</th>" +
-      "    <th scope='col'>Alerta</th>" +
-      "  </tr>" +
-      "</thead>  ";
-  
-    console.log(data)
-    for (var i = 0; i < data.length; i++) {
-      //console.log(data);
-      var atributos = ({ fecha,text } = data[i]);
-      var myDate;
-  
-      /*if(isNaN(atributos.created_at)){
+
+  var stamm = "https://arcgis-web.url.edu.gt/incyt/api/clima";
+  //var stamm = "http://localhost:3000/incyt/api/sosguate";
+
+  var url = stamm + "/getalertsdetailreport" + "?id=" + id;
+
+  $.get(url, function (data, status) {
+    //console.log("Data: " + data + "\nStatus: " + status);
+    if (data.length > 0) {
+      console.log("desplegamos grid");
+      //console.log(data[0].twitjson);
+      fillTable(data);
+    } else {
+      alert("No hay datos disponibles");
+    }
+  });
+}
+
+function fillTable(data) {
+  console.log(data);
+  var table = document.getElementById("tableInfo");
+  var tableTitle = document.getElementById("tableTitle");
+  tableTitle.innerHTML = "Reportes Municipales de Agua";
+  table.innerHTML = "";
+  table.innerHTML =
+    "<thead>" +
+    "   <tr>" +
+    "    <th scope='col'>Fecha</th>" +
+    "    <th scope='col'>Alerta</th>" +
+    "  </tr>" +
+    "</thead>  ";
+
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    //console.log(data);
+    var atributos = ({ fecha, text } = data[i]);
+    var myDate;
+
+    /*if(isNaN(atributos.created_at)){
         console.log(atributos.created_at + " is not a number ");
         myDate = atributos.created_at;
        }else{
@@ -197,23 +214,23 @@ function reporte(id) {
         myDate = new Date(1000 * atributos.created_at);
         console.log(myDate);
        }*/
-      
-      console.log(atributos);
-  
-      // IM WEBSITE ANSEHEN
-      var row = table.insertRow(i + 1);
-      var cell0 = row.insertCell(0);
-      var cell1 = row.insertCell(1);
-      //var cell2 = row.insertCell(2);
-      //var cell3 = row.insertCell(3);
-      cell0.innerHTML = atributos.fecha;
-      cell1.innerHTML = atributos.text;
-      //cell2.innerHTML = myDate;//atributos.created_at;
-      //cell3.innerHTML = atributos.source;
-    }
+
+    console.log(atributos);
+
+    // IM WEBSITE ANSEHEN
+    var row = table.insertRow(i + 1);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    //var cell2 = row.insertCell(2);
+    //var cell3 = row.insertCell(3);
+    cell0.innerHTML = atributos.fecha;
+    cell1.innerHTML = atributos.text;
+    //cell2.innerHTML = myDate;//atributos.created_at;
+    //cell3.innerHTML = atributos.source;
   }
-  $(document).ready(function () {
-    var ctx = document.getElementById('canvas').getContext('2d');
+}
+$(document).ready(function () {
+  var ctx = document.getElementById("canvas").getContext("2d");
 
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -232,9 +249,23 @@ function reporte(id) {
     this.yyyy2 +
     " visualizacion " +
     this.selectVisualizacion;
-    fetchData(this.estacion,this.estacion2,this.yyyy1,this.yyyy2,this.selectVisualizacion,ctx);
-  });
 
-
-
-  
+  if (this.estacion2.length > 1)
+    fetchData(
+      this.estacion,
+      this.estacion2,
+      this.yyyy1,
+      this.yyyy2,
+      this.selectVisualizacion,
+      ctx
+    );
+  else
+    fetchData2(
+      this.estacion,
+      this.estacion2,
+      this.yyyy1,
+      this.yyyy2,
+      this.selectVisualizacion,
+      ctx
+    );
+});
