3c9e140db49dae648521fc3bd05c1fad0370e645
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-10-23 19:40:57 -0600

    finished temp and rain projections

--- python/proyecciones.py
+++ python/proyecciones.py
@@ -1,6 +1,4 @@
-import tweepy, psycopg2, os, json, datetime, sys, requests, time
-import pandas as pd
-import numpy as np
+import  psycopg2,  json, datetime, sys, requests, time
 
 #connstr para bd
 #dev str
@@ -8,7 +6,7 @@ conn_string = "host='localhost' dbname='clima' user='postgres' password='Guatema
 
 #produccion str
 #conn_string = "host='localhost' dbname='clima' user='postgres' password='postgres2020!Incyt'"
-prediccion = 5
+prediccion = 5 #esto es el num. de anios a proyectar a futuro
 
 def getProcessDate():
     try:
@@ -40,7 +38,7 @@ def getLocation():
 
 def ejecutaComandoPsql(query):
     try:
-        print(query)
+        #print(query)
         conn = psycopg2.connect(conn_string)
         cursor = conn.cursor()
         cursor.execute(query)
@@ -61,7 +59,7 @@ def cargaUltimoAnio(estacion):
         m = row[0]
         #print(row[0],row[1])
     conn.close()
-    print(m)
+    #print(m)
     return m
 
 
@@ -95,7 +93,25 @@ def getAVGLluvia(estacion,mes):
         m.append([row[0],row[1],row[2],row[3],0])
         #print(([row[0],row[1],row[2],row[3],row[4]]))
     conn.close()
-    print(m)
+    #print(m)
+    return m
+
+
+def getAVGTemperatura(estacion,mes):
+    m = []
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+
+    q = " select estacion,year,mes,round(avg(TMAX),5) as \"tMax\",round(avg(Tmin),5) as \"tMin\", round( (avg(tmax) + avg(tmin))  / 2,1) as \"tPromedio\" from historico_estaciones where tmax >=-10 and tmin >=-10 and mes = " + str(mes) + " and estacion = '" + str(estacion) + "' group by estacion,year,mes order by mes,year "
+
+    cursor.execute(q)
+    rows = cursor.fetchall()
+    for row in rows:
+        #m.append([row[0],row[1].upper()])
+        m.append([row[0],row[1],row[2],row[3],row[4],row[5],0,0,0])
+        #print(([row[0],row[1],row[2],row[3],row[4]]))
+    conn.close()
+    #print(m)
     return m
 
 
@@ -118,8 +134,8 @@ def cargaLLuviaPorDia():
     arr.append([1,2,3])
     arr.append([4,5,6])
     np_arr = np.array(arr)
-    print(arr)
-    print(np_arr)
+    #print(arr)
+    #print(np_arr)
     
 
 def calculaProyeccionAbsoluto(ultimoAnio, datosLluvia, estacion):
@@ -133,7 +149,7 @@ def calculaProyeccionAbsoluto(ultimoAnio, datosLluvia, estacion):
             p.append([estacion,anio,mes,dia])
             
 
-def proyeccionAbsoluta(lista,ultimoAnio):
+def proyeccionAbsolutaLluvia(lista,ultimoAnio):
     #print('proyectando')
     i = 1
     promedio = 0
@@ -206,11 +222,120 @@ def ejecutaComandoPsql(query):
     except:
         write("error en ejecutar comando psql")
 
+
+def proyeccionAbsolutaTemperatura(lista,ultimoAnio):
+    #print('proyectando')
+    #['La_Fragua', Decimal('2018'), Decimal('1'), Decimal('30.13226'), Decimal('18.55806'), Decimal('24.3'), 0, 0, 0]
+    i = 1
+    promedioMin = 0
+    promedioMax = 0
+    promedioAvg = 0
+    while (i < len(lista)-1):
+        lista[i][6] = lista[i][3] - lista[i - 1][3]
+        lista[i][7] = lista[i][4] - lista[i - 1][4]
+        lista[i][8] = lista[i][5] - lista[i - 1][5]
+        promedioMax += lista[i][6]
+        promedioMin += lista[i][7]
+        promedioAvg += lista[i][8]
+        #print(lista[i]);
+        i+=1
+    promedioMin = promedioMin / len(lista)
+    promedioMax = promedioMax / len(lista)
+    promedioAvg = promedioAvg / len(lista)
+    #print(promedioMin,promedioMax,promedioAvg)
+    estacion = lista[i][0]
+    mes = lista[i][2]
+    ultimoValorMax = lista[i][3]
+    ultimoValorMin = lista[i][4]
+    ultimoValorAvg = lista[i][5]
+    i = 1
+    futuro = []
+    
+    while ( i <= prediccion):
+        #y = (ultimoAnio) + 100
+        #print(estacion)
+        #print(y)
+        #print(mes)
+        #print(ultimoValor + promedio)
+        futuro.append([estacion ,ultimoAnio + i, mes, ultimoValorMax + promedioMax, ultimoValorMin + promedioMin, ultimoValorAvg + promedioAvg])
+        ultimoValorMin = ultimoValorMin + promedioMin
+        ultimoValorMax = ultimoValorMax + promedioMax
+        ultimoValorAvg = ultimoValorAvg + promedioAvg
+        i += 1
+    #print(futuro)
+    return futuro
+
+
+def proyeccionPorcentualTemperatura(lista,ultimoAnio):
+    #print('proyectando')
+    #['La_Fragua', Decimal('2018'), Decimal('1'), Decimal('30.13226'), Decimal('18.55806'), Decimal('24.3'), 0, 0, 0]
+    i = 1
+    promedioMin = 0
+    promedioMax = 0
+    promedioAvg = 0
+    while (i < len(lista)-1):
+        lista[i][6] = (lista[i][3] - lista[i - 1][3])/lista[i - 1][3]
+        lista[i][7] = (lista[i][4] - lista[i - 1][4])/lista[i - 1][4]
+        lista[i][8] = (lista[i][5] - lista[i - 1][5])/lista[i - 1][5]
+        promedioMax += lista[i][6]
+        promedioMin += lista[i][7]
+        promedioAvg += lista[i][8]
+        #print(lista[i]);
+        i+=1
+    promedioMin = promedioMin / len(lista)
+    promedioMax = promedioMax / len(lista)
+    promedioAvg = promedioAvg / len(lista)
+    #print(promedioMin,promedioMax,promedioAvg)
+    estacion = lista[i][0]
+    mes = lista[i][2]
+    ultimoValorMax = lista[i][3]
+    ultimoValorMin = lista[i][4]
+    ultimoValorAvg = lista[i][5]
+    i = 1
+    futuro = []
+    
+    while ( i <= prediccion):
+        #y = (ultimoAnio) + 100
+        #print(estacion)
+        #print(y)
+        #print(mes)
+        #print(ultimoValor + promedio)
+        futuro.append([estacion ,ultimoAnio + i, mes, ultimoValorMax + promedioMax, ultimoValorMin + promedioMin, ultimoValorAvg + promedioAvg])
+        ultimoValorMin = ultimoValorMin + promedioMin
+        ultimoValorMax = ultimoValorMax + promedioMax
+        ultimoValorAvg = ultimoValorAvg + promedioAvg
+        i += 1
+    #print(futuro)
+    return futuro
+
+
     
 if __name__ == "__main__":
   print('inicia proceso de proyecciones')
   estaciones = cargaEstaciones()
 
+#temperatura
+  for e in estaciones:
+    print(e)
+    for x in range(1, 12):
+      lista = getAVGTemperatura(e,x)
+      ultimoAnio = cargaUltimoAnio(e)
+          
+      futuroAbsoluto = proyeccionAbsolutaTemperatura(lista,ultimoAnio)
+      #print(futuroAbsoluto)
+      for f in futuroAbsoluto:
+          q = 'insert into proyeccion_absoluta_Temperatura (estacion,anio,mes,proyeccion_max,proyeccion_min,proyeccion_avg) values (\'' + str(f[0]) + '\','+ str(f[1]) + ',' + str(f[2]) + ',' + str(f[3]) +  ',' + str(f[4])+ ',' + str(f[5]) + ' )'
+          ejecutaComandoPsql(q)
+    
+      futuroAbsoluto = proyeccionPorcentualTemperatura(lista,ultimoAnio)
+      for f in futuroAbsoluto:
+          q = 'insert into proyeccion_porcentual_Temperatura (estacion,anio,mes,proyeccion_max,proyeccion_min,proyeccion_avg) values (\'' + str(f[0]) + '\','+ str(f[1]) + ',' + str(f[2]) + ',' + str(f[3]) +  ',' + str(f[4])+ ',' + str(f[5]) + ' )'
+          ejecutaComandoPsql(q)
+
+
+
+
+#lluvia
   for e in estaciones:
     print(e)
     for x in range(1, 12):
@@ -218,7 +343,7 @@ if __name__ == "__main__":
       ultimoAnio = cargaUltimoAnio(e)
 
 
-      futuroAbsoluto = proyeccionAbsoluta(lista,ultimoAnio)
+      futuroAbsoluto = proyeccionAbsolutaLluvia(lista,ultimoAnio)
       for f in futuroAbsoluto:
           q = 'insert into proyeccion_absoluta_lluvia (estacion,anio,mes,proyeccion) values (\'' + str(f[0]) + '\','+ str(f[1]) + ',' + str(f[2]) + ',' + str(f[3]) + ' )'
           ejecutaComandoPsql(q)
@@ -227,10 +352,19 @@ if __name__ == "__main__":
       for f in futuroPorcentual:
           q = 'insert into proyeccion_porcentual_lluvia (estacion,anio,mes,proyeccion) values (\'' + str(f[0]) + '\','+ str(f[1]) + ',' + str(f[2]) + ',' + str(f[3]) + ' )'
           ejecutaComandoPsql(q)
-    
-    
+
+
+
+print('fin de proyeccion')
+
+
+
 
 '''
+
+
+
+
 
 select estacion,year,mes,round(avg(lluvia),5) as "avgLluvia"
 from historico_estaciones
@@ -238,6 +372,17 @@ where lluvia >=-10
 and mes = " + str(mes) + "
 and estacion = '" + estacion + "'
 group by estacion,year,mes order by mes,year 
+
+
+
+select estacion,year,mes,round(avg(TMAX),5) as "tMax",round(avg(Tmin),5) as "tMin",
+round( (avg(tmax) + avg(tmin))  / 2,1) as "tPromedio"
+from historico_estaciones where tmax >=-10 and tmin >=-10 and mes = 12
+and estacion = 'INSIVUMEH' group by estacion,year,mes order by mes,year
+
+
+
+
 
 
 
