383bd11efcd2d69a4082b29f4eca1d5b4c7ef318
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-10-24 18:22:50 -0600

    layout for projection

--- climaJavaVersion/proyeccionTemperatura.js
+++ climaJavaVersion/proyeccionTemperatura.js
@@ -7,8 +7,8 @@ var estacion2;
 var yyyy1;
 var yyyy2;
 var selectVisualizacion;
-var data;
-var data2;
+var dataAbsoluto;
+var dataPorcentual;
 //graphs
 var labels;
 var datasetLluvia;
@@ -16,6 +16,79 @@ var color = Chart.helpers.color;
 
 var campos;
 var arreglo;
+
+
+async function loadData(estacion){
+    var res = await fetch(stamm + "/proyeccionAbsolutaTemperatura?estacion=" + estacion);
+    this.dataAbsoluto = await res.json();
+    console.log(this.dataAbsoluto);
+
+    res = await fetch(stamm + "/proyeccionPorcentualTemperatura?estacion=" + estacion);
+    this.dataPorcentual = await res.json();
+    console.log(this.dataPorcentual);
+
+    var lAbsoluta = [];
+    var lPorcentual = [];
+    
+    for (var i = 0; i < this.dataAbsoluto.length; i++) {
+      lAbsoluta.push( this.dataAbsoluto[i].anio + "/" + this.dataAbsoluto[i].mes );
+    }
+
+    for (var i = 0; i < this.dataPorcentual.length; i++) {
+      lPorcentual.push( this.dataPorcentual[i].anio + "/" + this.dataPorcentual[i].mes );
+    }
+
+    
+
+    
+  }
+
+function download_csv() {
+  var archivo = prompt(
+    "Ingrese el nombre del archivo a salvar:",
+    "archivo_datos.csv"
+  );
+  if (archivo == null || archivo == "") {
+    console.log("User cancelled the prompt.");
+  } else {
+    //DOWNLOADING FILE
+    //archivo = archivo.replace(/ /g,'');//removing white spaces from file name
+    if (archivo.toUpperCase().indexOf(".CSV") == -1) {
+      archivo = archivo + ".csv";
+    }
+    console.log(archivo);
+    console.log(campos);
+    console.log(arreglo);
+    //here i create the csv.
+    var data = [
+      ["Foo", "programmer"],
+      ["Bar", "bus driver"],
+      ["Moo", "Reindeer Hunter"],
+    ];
+    var csv = "";
+
+    //set titles
+    for (var i = 0; i < campos.length; i++) {
+      csv += campos[i] + ",";
+    }
+    //set data
+    for (var i = 0; i < arreglo.length; i++) {
+      csv += "\n";
+      for (var j = 0; j < campos.length; j++) {
+        csv += arreglo[i][campos[j]] + ",";
+      }
+    }
+    csv += "\n";
+
+    console.log(csv);
+    var hiddenElement = document.createElement("a");
+    hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
+    hiddenElement.target = "_blank";
+    hiddenElement.download = archivo; //"people.csv";
+    hiddenElement.click();
+  }
+}
+
 
 
 $(document).ready(function () {
@@ -29,13 +102,8 @@ $(document).ready(function () {
     this.yyyy2 = urlParams.get("selectYYYY2");
     this.selectVisualizacion = urlParams.get("selectVisualizacion");
   
-  
-    if (this.estacion2.length > 1) {
-     
-    } else {
-      
-
-    }
+    loadData(this.estacion);
+    
   });
   
   
